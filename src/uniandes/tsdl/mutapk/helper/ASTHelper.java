9d225deb79598ded2b0933d1067c4a5d78e8b1df
Camilo Andrés Escobar Velásquez <ca.escobar2434@uniandes.edu.co>
2018-10-30 11:22:10 -0500

    BLUETOOTH_ADAPTER_ALWAYS_AVAILABLE FirstApproach

--- src/uniandes/tsdl/mutapk/helper/ASTHelper.java
+++ src/uniandes/tsdl/mutapk/helper/ASTHelper.java
@@ -115,12 +115,14 @@ public class ASTHelper {
 	}
 
 	public static int[] isValidLocation(CommonTree t){
-//		if(t.getType()==159) {
+//		if(t.getType()==161 && t.getChild(2).toStringTree().equals("Landroid/bluetooth/BluetoothAdapter;")
+//				&& t.getChild(3).toStringTree().equals("isEnabled")
+//				&& getFirstBrotherNamedOfType(smaliParser.I_STATEMENT_FORMAT21t, "if-eqz", t).getLine()<t.getLine()+5) {
 //			System.out.println(t.toStringTree());
 //			System.out.println(t.getType());
-//			System.out.println(t.getChild(2));
-//			System.out.println(t.getChild(3));
-//			System.out.println(t.getChild(2).toStringTree().equals("Ljava/net/URI;") && t.getChild(3).toStringTree().equals("<init>") );
+//			//			System.out.println(t.getChild(2));
+//			//			System.out.println(t.getChild(3));
+//			//			System.out.println(t.getChild(2).toStringTree().equals("Ljava/net/URI;") && t.getChild(3).toStringTree().equals("<init>") );
 //		}
 
 		if(t.getType()==159) {
@@ -143,25 +145,32 @@ public class ASTHelper {
 			} else if (t.getText().equals("findViewById")) {
 				return new int[]{27, 31};	
 			}
+		} else if(t.getType()==161) {
+			if(t.getChild(2).toStringTree().equals("Landroid/bluetooth/BluetoothAdapter;")
+					&& t.getChild(3).toStringTree().equals("isEnabled")
+					&& getFirstBrotherNamedOfType(smaliParser.I_STATEMENT_FORMAT21t, "if-eqz", t) != null
+					&& getFirstBrotherNamedOfType(smaliParser.I_STATEMENT_FORMAT21t, "if-eqz", t).getLine()<(t.getLine()+5)) {
+				return new int[]{15};
+			}
 		}
 
-//		if(t.getType()==159 
-//				&& t.getFirstChildWithType(smaliParser.I_REGISTER_LIST).getChildCount()==3 
-//				&& t.getFirstChildWithType(smaliParser.CLASS_DESCRIPTOR).toString().equals("Landroid/content/Intent;") 
-//				&& t.getFirstChildWithType(smaliParser.SIMPLE_NAME).toString().equals("<init>")){
-//			return new int[]{2, 6};
-//		} else if (t.getType()==191 && t.getText().equals("putExtra")){ //InvalidKeyIntentPutExtra && NullValueIntentPutExtra
-//			return new int[]{4, 7}; 
-//		} else if (t.getType()==191 && t.getText().equals("findViewById") && hasIPutAndIGet(t)!=null) {
-//			return new int[]{26, 29};
-//		} else if (t.getType()==191 && t.getText().equals("findViewById")) {
-//			return new int[]{27, 31};	
-//		} else if (t.getType()==159 && t.getChild(2).toStringTree().equals("Ljava/io/File;") 
-//				&& t.getChild(4).getChild(1)!=null && t.getChild(4).getChild(1).toStringTree().equals("Ljava/lang/String;")){//File.<init>
-//			return new int[]{32};	
-//		} else if(t.getType()==159 && ){//URI.<init>
-//			return new int[]{17};
-//		} 
+		//		if(t.getType()==159 
+		//				&& t.getFirstChildWithType(smaliParser.I_REGISTER_LIST).getChildCount()==3 
+		//				&& t.getFirstChildWithType(smaliParser.CLASS_DESCRIPTOR).toString().equals("Landroid/content/Intent;") 
+		//				&& t.getFirstChildWithType(smaliParser.SIMPLE_NAME).toString().equals("<init>")){
+		//			return new int[]{2, 6};
+		//		} else if (t.getType()==191 && t.getText().equals("putExtra")){ //InvalidKeyIntentPutExtra && NullValueIntentPutExtra
+		//			return new int[]{4, 7}; 
+		//		} else if (t.getType()==191 && t.getText().equals("findViewById") && hasIPutAndIGet(t)!=null) {
+		//			return new int[]{26, 29};
+		//		} else if (t.getType()==191 && t.getText().equals("findViewById")) {
+		//			return new int[]{27, 31};	
+		//		} else if (t.getType()==159 && t.getChild(2).toStringTree().equals("Ljava/io/File;") 
+		//				&& t.getChild(4).getChild(1)!=null && t.getChild(4).getChild(1).toStringTree().equals("Ljava/lang/String;")){//File.<init>
+		//			return new int[]{32};	
+		//		} else if(t.getType()==159 && ){//URI.<init>
+		//			return new int[]{17};
+		//		} 
 
 
 		//		} else if (t.) {
