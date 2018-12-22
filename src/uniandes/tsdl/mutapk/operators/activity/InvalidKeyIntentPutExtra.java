fd8213effa2b5ef7f071cd6db2a90b073c0bbca3
Camilo Andrés Escobar Velásquez <ca.escobar2434@uniandes.edu.co>
2018-12-22 17:43:53 -0500

    First approach to solve MO4 compilation errors

--- src/uniandes/tsdl/mutapk/operators/activity/InvalidKeyIntentPutExtra.java
+++ src/uniandes/tsdl/mutapk/operators/activity/InvalidKeyIntentPutExtra.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.antlr.runtime.tree.CommonTree;
 
+import uniandes.tsdl.antlr.smaliParser;
 import uniandes.tsdl.mutapk.helper.FileHelper;
 import uniandes.tsdl.mutapk.helper.Helper;
 import uniandes.tsdl.mutapk.helper.StringGenerator;
@@ -20,7 +21,6 @@ public class InvalidKeyIntentPutExtra implements MutationOperator{
 		
 		ASTMutationLocation mLocation = (ASTMutationLocation) location;
         CommonTree parent = (CommonTree) mLocation.getTree().getParent();
-//        System.out.println(parent.toStringTree());
         List<CommonTree> hijos = (List<CommonTree>)parent.getChildren();
         String extraKeyVar = hijos.get(1).getChild(1).toString();
         CommonTree method = (CommonTree) parent.getParent();
@@ -29,11 +29,12 @@ public class InvalidKeyIntentPutExtra implements MutationOperator{
         boolean finished = false;
         for (int i = putExtraPos; i >-1 && !finished; i--) {
         	CommonTree temp = (CommonTree) method.getChild(i);
-        	if(temp.getType()==140 && temp.getChild(1).toString().equals(extraKeyVar)) {
+        	if(temp.getType()==smaliParser.I_STATEMENT_FORMAT21c_STRING && temp.getChild(1).toString().equals(extraKeyVar)) {
         		extraKeyValPos = temp.getLine();
         		finished = true;
         	}
 		}
+        
         String newKey = StringGenerator.generateRandomString();
         
         List<String> newLines = new ArrayList<String>();
