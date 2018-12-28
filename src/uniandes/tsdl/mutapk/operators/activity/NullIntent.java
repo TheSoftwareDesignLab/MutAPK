35592b7b1b0b6a2dddc1bcc0b373a1b808ac405c
Camilo Andrés Escobar Velásquez <ca.escobar2434@uniandes.edu.co>
2018-12-28 18:38:01 -0500

    MO6 compilation errors Solved

--- src/uniandes/tsdl/mutapk/operators/activity/NullIntent.java
+++ src/uniandes/tsdl/mutapk/operators/activity/NullIntent.java
@@ -22,35 +22,7 @@ public class NullIntent implements MutationOperator{
 		ASTMutationLocation mLocation = (ASTMutationLocation) location;
 		CommonTree parent = (CommonTree) mLocation.getTree().getParent();
 		List<CommonTree> hijos = (List<CommonTree>)parent.getChildren();
-//		int intentInstanceLine = -1;
 		String intentInstanceLineVar = mLocation.getTree().getChild(1).getChild(0).toString();
-//		
-//		int contextInstanceLine = -1;
-//		String contextInstanceLineVar = mLocation.getTree().getChild(1).getChild(1).toString();
-//		
-//		int classComponentLine = -1;
-//		
-//		String classComponentLineVar = mLocation.getTree().getChild(1).getChild(2).toString();
-//		int actualChild = mLocation.getTree().getChildIndex();
-//		while(actualChild>0 && (intentInstanceLine == -1 || contextInstanceLine == -1 || classComponentLine == -1)) {
-//			CommonTree temp = (CommonTree) hijos.get(actualChild);
-//			List<CommonTree> hijoss = (List<CommonTree>) temp.getChildren();
-//			if(temp.getType()==142 
-//					&& hijoss.get(0).getText().equals("const-class")
-//					&& hijoss.get(1).getText().equals(classComponentLineVar)){
-//				classComponentLine = temp.getLine();
-//			} else if(temp.getType()==142 
-//					&& hijoss.get(0).getText().equals("new-instance")
-//					&& hijoss.get(1).getText().equals(intentInstanceLineVar)){
-//				intentInstanceLine = temp.getLine();
-//			} else if(temp.getType()==148
-//					&& hijoss.get(0).getText().equals("iget-object")
-//					&& hijoss.get(1).getText().equals(contextInstanceLineVar)) {
-//				contextInstanceLine = temp.getLine();
-//			}
-//			
-//			actualChild--;
-//		}
 		
 		List<String> newLines = new ArrayList<String>();
 		List<String> lines = FileHelper.readLines(location.getFilePath());
@@ -64,22 +36,6 @@ public class NullIntent implements MutationOperator{
 		
 		//Apply mutation
 		newLines.add("    const/4 "+intentInstanceLineVar+", 0x0");
-//		String linesToConsider = "";
-//		boolean newLineFlag = false;
-//		for(int i = location.getStartLine(); i <= location.getEndLine(); i++){
-//			if(newLineFlag){
-//				linesToConsider += " "+lines.get(i);
-//			} else {
-//				linesToConsider += lines.get(i);
-//				newLineFlag = true;
-//			}
-//		}
-//		
-//		String sub1 = linesToConsider.substring(0, location.getStartColumn());
-//		String sub2 = linesToConsider.substring(location.getEndColumn());
-//		
-//		String mutatedLine = sub1 + "null"  + sub2;
-//		newLines.add(mutatedLine);
 		
 		
 		//Add lines after the MutationLocation
