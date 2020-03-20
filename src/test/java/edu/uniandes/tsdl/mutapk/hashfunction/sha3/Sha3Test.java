e56083e1c199818be0ee06b8dbfae4efda8cb658
Diego <df.riveros11@uniandes.edu.co>
2020-03-20 17:20:23 -0500

    oli

--- src/test/java/edu/uniandes/tsdl/mutapk/hashfunction/sha3/Sha3Test.java
+++ src/test/java/edu/uniandes/tsdl/mutapk/hashfunction/sha3/Sha3Test.java
@@ -11,36 +11,36 @@ import org.junit.Test;
 public class Sha3Test {
 
 	
-	String apkAbsolutePath = "C:\\Users\\PERSONAL\\git\\MutAPK\\test\\temp";
-	File manifest = new File(apkAbsolutePath + File.separator + "AndroidManifest.xml");
-	File smali = new File(apkAbsolutePath + File.separator + "smali");
-	File resource = new File(apkAbsolutePath + File.separator + "res");
-	
-	@Test
-	public void equalHashTest() throws FileNotFoundException, IOException {
-		String hashManifest = Sha3.sha512FileSeparte(manifest);
-		String hashSmali = Sha3.sha512FileSeparte(smali);
-		String hashResource = Sha3.sha512FileSeparte(resource);
-		
-		String hashManifestCompare = "";
-		String hashSmaliCompare = "";
-		String hashResourceCompare = "";
-		
-		for (int i = 0; i < 10; i++) {
-			hashManifestCompare = Sha3.sha512FileSeparte(manifest);
-			hashSmaliCompare =  Sha3.sha512FileSeparte(smali);
-			hashResourceCompare = Sha3.sha512FileSeparte(resource);
-			
-			if(!hashManifest.equals(hashManifestCompare)) {
-				fail("They should be always equals");
-			}
-			if(!hashSmali.equals(hashSmaliCompare)) {
-				fail("They should be always equals");
-			}
-			if(!hashResource.equals(hashResourceCompare)) {
-				fail("They should be always equals");
-			}
-		}
-	}
+//	String apkAbsolutePath = "C:\\Users\\PERSONAL\\git\\MutAPK\\test\\temp";
+//	File manifest = new File(apkAbsolutePath + File.separator + "AndroidManifest.xml");
+//	File smali = new File(apkAbsolutePath + File.separator + "smali");
+//	File resource = new File(apkAbsolutePath + File.separator + "res");
+//	
+//	@Test
+//	public void equalHashTest() throws FileNotFoundException, IOException {
+//		String hashManifest = Sha3.sha512FileSeparte(manifest);
+//		String hashSmali = Sha3.sha512FileSeparte(smali);
+//		String hashResource = Sha3.sha512FileSeparte(resource);
+//		
+//		String hashManifestCompare = "";
+//		String hashSmaliCompare = "";
+//		String hashResourceCompare = "";
+//		
+//		for (int i = 0; i < 10; i++) {
+//			hashManifestCompare = Sha3.sha512FileSeparte(manifest);
+//			hashSmaliCompare =  Sha3.sha512FileSeparte(smali);
+//			hashResourceCompare = Sha3.sha512FileSeparte(resource);
+//			
+//			if(!hashManifest.equals(hashManifestCompare)) {
+//				fail("They should be always equals");
+//			}
+//			if(!hashSmali.equals(hashSmaliCompare)) {
+//				fail("They should be always equals");
+//			}
+//			if(!hashResource.equals(hashResourceCompare)) {
+//				fail("They should be always equals");
+//			}
+//		}
+//	}
 
 }
